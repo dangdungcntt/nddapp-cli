b7a2fd36425f68613794a6b3abed8b9f2452ec96
Dung Nguyen Dang <dangdungcntt@gmail.com>
2022-06-03 14:16:46 +0700

    Added SqlToMongoDbQuery

---
+++ app/Requests/SqlToMongoDbRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Requests;
+
+use App\Requests\Contracts\RequestInterface;
+
+class SqlToMongoDbRequest implements RequestInterface
+{
+    public function __construct(
+        protected string $sql
+    ) {
+    }
+
+    public function body(): array
+    {
+        return [
+            'inputs' => [
+                'sql' => $this->sql,
+            ]
+        ];
+    }
+
+    public function path(): string
+    {
+        return '/sql-to-mongodb-query-converter.html';
+    }
+
+    public function method(): string
+    {
+        return 'POST';
+    }
+}
---
+++ app/Responses/SqlToMongoDbQueryResponse.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Responses;
+
+class SqlToMongoDbQueryResponse extends CommonStringResponse
+{
+    public function toPrettyHtml(): string
+    {
+        if ($this->succeed) {
+            $html = json_encode(json_decode($this->content), JSON_PRETTY_PRINT);
+            return <<<HTML
+                <div class="text-green-400"><pre>$html</pre></div>
+HTML;
+        }
+
+        return parent::toHtml();
+    }
+}

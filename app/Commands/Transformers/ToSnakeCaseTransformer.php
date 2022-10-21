2a22d7e911b7f4ffe56abc55e104346937ab1aac
Dung Nguyen Dang <dangdungcntt@gmail.com>
2022-10-21 14:06:32 +0700

    feat: snake support --d|dot option to convert dot (.) to underscore (_)

--- app/Commands/Transformers/ToSnakeCaseTransformer.php
+++ app/Commands/Transformers/ToSnakeCaseTransformer.php
@@ -7,12 +7,17 @@ use Illuminate\Support\Str;
 
 class ToSnakeCaseTransformer extends CommonStringProcessor
 {
-    protected $signature = 'snake {string}';
-
+    protected $signature = 'snake {string} {--d|dot}';
     protected $description = 'Convert string to snake case';
 
     public function process(string $string, array $options): string
     {
+        $string = trim($string);
+
+        if (isset($options['dot']) && $options['dot']) {
+            $string = str_replace('.', '_', $string);
+        }
+
         return Str::snake(trim($string));
     }
 }
